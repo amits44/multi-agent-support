2ba3337de535fa86b00c77e092e802c6fc11ecce
Amit <amitshantijyala2004@gmail.com>
2026-04-17 16:20:47 +0530

    general agent

--- agents/general_agent.py
+++ agents/general_agent.py
@@ -33,12 +33,60 @@ class GeneralAgent:
             print(f"error parsing knowledge base {e}")
             return []
     
+    def _create_vector_store(self):
+        if not self.knowledge_base:
+            print("no knowledg base found")
+            return None
+        try:
+            documents =[]
+            for item in self.knowledge_base:
+                text = f"Question: {item['question']}\n Answers: {item['answer']}"
+                doc = documents(
+                    page_content = text,
+                    metadata={
+                        'id': item.get('id','')
+                        'category': item.get('category','')
+                        'question': item.get('question','')
+                        'answer': item.get('answer','')
+                    }
+                )
+                documents.append(doc)
+            vector_store = InMemoryVectorStore(
+                documents= documents,
+                embedding= self.embeddings
+            )
+            print(f"vector store created containing {len(documents)} documents")
+            return vector_store
+        
+        except Exception as e:
+            print("failed to create vector store{e}")
+            return None
+
     def search_knowledge_base(self, query: str, top_k: int =3)-> List[Dict]:
 
-        embed = HuggingFaceEmbeddings(model="sentence-transformers/all-MiniLM-L6-v2")
-        vector_store = InMemoryVectorStore.from_documents(
-            embdeeing = embed,
-            documents = kb_path)
-        similar_docs = vector_store.similarity_search("your query here")
-        return similar_docs
+        if self.vector_store:
+            try:
+                similar_docs = self.vector_store.similarity_search(
+                    query = query,
+                    k = top_k
+                )
+                results=[]
+                for docs in similar_docs:
+                    results.append({
+                        'id' : docs.metadata.get('id',''),
+                        'category': docs.metadata.get('category',''),
+                        'question': docs.metadata.get('question',''),
+                        'answer': docs.metadata.get('answer','')
+                    })
+                print(f"found {len(results)} through vector search")
+                return results
+            
+            except Exception as e:
+                print(f"vector search failed {e}")
+        return self._keyword_search(query, top_k)
+    
+    def _keyword_search(self, query:str, top_k: int =3):
+        if not self.knowledge_base:
+            return None
+        
 
